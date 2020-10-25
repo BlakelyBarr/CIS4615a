cc23b07c1a1fa36e169daaac1ead186376d60413
BlakelyBarr <46506594+BlakelyBarr@users.noreply.github.com>
2020-10-25 19:12:38 -0400

    Update R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -2,8 +2,8 @@
  *  Compilation:  javac R04_STR03_J.java
  *  Execution:    java R04_STR03_J
  *
- *  Attempts to convert a BigInteger value to a String and then restore it to a BigInteger value
- *
+ *  Produces a String representation of the BigInteger object and then converts the String object to a byte array
+ *  This process is then reversed
  * Revision History:
  * - Attempts to convert a BigInteger value to a String and then restore it to a BigInteger value
  *  % java R04_STR03_J
@@ -13,13 +13,13 @@
 
 public class R04_STR03_J {
     
-    // Attempts to convert a BigInteger value to a String and then restore it to a BigInteger value
-    // Converting the String back to a BigInteger is unlikely to reproduce the original value
+    // Produces a String representation of the BigInteger object and then converts the String object to a byte array
+    // This process is then reversed
     
     BigInteger x = new BigInteger("530500452766");
-    byte[] byteArray = x.toByteArray();
-    String s = new String(byteArray);
-    byteArray = s.getBytes();
-    x = new BigInteger(byteArray);
+    String s = x.toString();  // Valid character data
+    byte[] byteArray = s.getBytes();
+    String ns = new String(byteArray); 
+    x = new BigInteger(ns);
 
 }
