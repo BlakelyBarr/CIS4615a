0f8d6bec3bfd5e1ef69c383745fab7bb938343a6
BlakelyBarr <46506594+BlakelyBarr@users.noreply.github.com>
2020-10-25 19:09:03 -0400

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -2,10 +2,10 @@
  *  Compilation:  javac R02_XP00_J.java
  *  Execution:    java R02_XP00_J
  *
- *  Logs unsanitized user input.
+ *  checks the Boolean value returned by the delete() method
  *
  * Revision History:
- * - Added method that does not check values returned by methods
+ * - checks the Boolean value returned by the delete() method
  *  % java R02_XP00_J
  *  %
  *
@@ -13,12 +13,15 @@
 
 public class R02_XP00_J {
     
+    //This compliant solution checks the Boolean value returned by the delete() method and handles any resulting errors:
     public void deleteFile(){
- 
-    File someFile = new File("someFileName.txt");
-    // Do something with someFile
-    someFile.delete();
- 
+
+      File someFile = new File("someFileName.txt");
+      // Do something with someFile
+      if (!someFile.delete()) {
+        // Handle failure to delete the file
+      }
+
     }
 
 }
